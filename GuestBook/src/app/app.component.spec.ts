eab8d9739c6a7f4153b5366fac918dbf041a8397
Andrey Rastorguev <xquzyx@yandex.ru>
2017-07-10 16:09:10 +0300

    Added component

--- GuestBook/src/app/app.component.spec.ts
+++ GuestBook/src/app/app.component.spec.ts
@@ -1,32 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
+﻿import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
 
-import { AppComponent } from './app.component';
-
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));
-});
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    constructor(private _httpService: Http) { }
+    apiValues: string[] = [];
+    ngOnInit() {
+        this._httpService.get('/api/values').subscribe(values => {
+            this.apiValues = values.json() as string[];
+        });
+    }
+}
